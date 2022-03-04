11062923aa422c029e7dd0b85d8242d3eb104e75
atefe fazeli <atefefazeli1996@gmail.com>
2022-03-04 20:04:16 +0330

    Navbar component created

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Hi! I Am<span> Atefe Fazeli</span></h1>
+        </div>
+    );
+};
+
+export default Landing;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <button>Contact Me</button>
+        </div>
+    );
+};
+
+export default Navbar;
