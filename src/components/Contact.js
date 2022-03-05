5d329c83a146008403815aa265aed48d06889ecc
atefe fazeli <atefefazeli1996@gmail.com>
2022-03-05 20:44:57 +0330

    contact component created

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href='#'>Telegram</a></li>
+                <li><a href='#'>Email</a></li>
+                <li><a href='#'>WhatsApp</a></li>
+                <li><a href='#'>Inestgram</a></li>
+            </ul>
+            
+        </div>
+    );
+};
+
+export default Contact;
