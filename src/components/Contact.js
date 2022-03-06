8c650096e42e60846f8988b9a8cd2103f8184e10
atefe fazeli <atefefazeli1996@gmail.com>
2022-03-06 20:31:31 +0330

    contact icons styles created

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from "./Contact.module.css";
-
+import {FaTelegram ,FaInstagram,FaWhatsapp} from "react-icons/fa";
+import {MdOutlineMailOutline} from "react-icons/md"; 
+import {RiTelegramLine} from "react-icons/ri";
 const Contact = () => {
     return (
         <div className={styles.container}>
             <ul className={styles.navbarlist}>
-                <li><a href='#'>Telegram</a></li>
-                <li><a href='#'>Email</a></li>
-                <li><a href='#'>WhatsApp</a></li>
-                <li><a href='#'>Inestgram</a></li>
+                <li><a href='#'> <RiTelegramLine className={styles.icon}/>Telegram</a></li>
+                <li><a href='#'><MdOutlineMailOutline className={styles.icon}/>Email</a></li>
+                <li><a href='#'><FaWhatsapp className={styles.icon}/>WhatsApp</a></li>
+                <li><a href='#'><FaInstagram className={styles.icon}/>Inestgram</a></li>
             </ul>
             
         </div>
