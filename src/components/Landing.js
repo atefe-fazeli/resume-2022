1f4dea39afeec55bf37295ec4b8959d28a9bbed3
atefe fazeli <atefefazeli1996@gmail.com>
2022-03-07 18:08:28 +0330

    landing component started

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './Landing.module.css';
+import mypic from '../images/mypic.png';
 const Landing = () => {
     return (
         <div className={styles.container}>
+
+            <div>
             <h1>Hi! I Am<span> Atefe Fazeli</span></h1>
+            </div>
+            <div>
+                <img src={mypic}/>
+            </div>
+            
+            
         </div>
     );
 };
