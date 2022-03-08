bfdd679e00acf1ac82d42e6326334f3f6ca60f1d
atefe fazeli <atefefazeli1996@gmail.com>
2022-03-08 19:30:12 +0330

    info component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Landing from './components/Landing';
 import Navbar from './components/Navbar';
 import Buttons from './components/Buttons';
+import Info from './components/Info';
 function App() {
   return (
     <div >
       <Navbar />
       <Landing />
+      <Info />
       <Buttons />
 
     </div>
---
+++ src/components/Info.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Info = () => {
+    return (
+        <div>
+            <div>
+                <h3>Profile</h3>
+                <p>Born : 1996 <br />
+                   Maritial status : 1996 <br />
+                   Phone Number: 09139118048 <br />
+                   Location : Tehran/North-Gorgan <br />
+                </p>
+            </div>
+            <div>
+                <h3>Education</h3>
+                <p>Graduated as a mining engineering from Amirkabir University of technology</p>
+            </div>
+            <div>
+                <h3>Language</h3>
+                <p>loarem</p>
+            </div>
+            <div>
+                <h3>Interests</h3>
+                <p>loarem</p>
+            </div>
+            <div>
+                <h3>Courses</h3>
+                <p>Milad Azami react course</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Info;
