19ecd8e52608625bf06426797a510759ff2682dc
atefe fazeli <atefefazeli1996@gmail.com>
2022-03-08 21:21:06 +0330

    info component styled

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,31 +1,36 @@
 import React from 'react';
-
+import styles from './Info.module.css';
+import { GoLocation } from "react-icons/go";
+import { GiNewBorn } from "react-icons/gi";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { GiBigDiamondRing } from "react-icons/gi";
 const Info = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <div>
-                <h3>Profile</h3>
-                <p>Born : 1996 <br />
-                   Maritial status : 1996 <br />
-                   Phone Number: 09139118048 <br />
-                   Location : Tehran/North-Gorgan <br />
+               <p>
+                  <span className={styles.titlehover}>Profile</span><br />
+                     <GiNewBorn/> Born : 1996 <br />
+                     <GiBigDiamondRing/> Maritial status : Single <br />
+                     <BsFillTelephoneFill/> Phone Number: 09139118048 <br />
+                     <GoLocation/> Location : Tehran/North-Gorgan <br />
                 </p>
             </div>
             <div>
-                <h3>Education</h3>
-                <p>Graduated as a mining engineering from Amirkabir University of technology</p>
+                <p><span className={styles.titlehover}>Education</span><br />
+                Graduated as a mining engineering from Amirkabir University of technology</p>
             </div>
             <div>
-                <h3>Language</h3>
-                <p>loarem</p>
+                <p><span className={styles.titlehover}>Language</span><br />
+                loarem</p>
             </div>
             <div>
-                <h3>Interests</h3>
-                <p>loarem</p>
+               <p><span className={styles.titlehover}>Interests</span><br />
+                loarem</p>
             </div>
             <div>
-                <h3>Courses</h3>
-                <p>Milad Azami react course</p>
+                <p> <span className={styles.titlehover}>Courses</span><br />
+                Milad Azami react course</p>
             </div>
             
         </div>
